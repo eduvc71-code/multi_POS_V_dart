3eb77a4eb05596ebe87d14c90296003ab68c18a2
eduvc71-code <eduvc71@gmail.com>
2026-08-27 02:35:21 -0400

    fix: Corrección definitiva de layout y null safety en Panel Principal

---
+++ .widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: implementation_imports
+
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:52001/6nB-1bTYkMk=';
+const String kWidgetPreviewService =
+    'widget-preview-802b4bdb-4190-414e-97ea-e084cb4720b9';
+const String kWidgetPreviewScaffoldStream =
+    'WidgetPreviewScaffold-802b4bdb-4190-414e-97ea-e084cb4720b9';
+const String kProjectRootPath = r'D:\multi-pos-1';
